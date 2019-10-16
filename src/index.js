c7b2fec5ccfd9f1721955fed2deaa782c950a5b4
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-10-16 14:49:59 +0100

    store fetched

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { combineReducers, createStore, compose, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from 'redux-thunk';
+import "./index.css";
+import App from "./App";
+import * as reducers from "./states/reducers";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const monsterReducer = combineReducers({
+  count: reducers.countReducer,
+  api: reducers.nasaAPI,
+});
+
+const store = createStore(
+  monsterReducer,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
